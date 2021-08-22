e425d3c935994df534321decdd2e9d7ed1607c43
Kaby Chow <kaby.chow@gmail.com>
2021-08-22 14:31:39 +0800

    update

--- model/bert.go
+++ model/bert.go
@@ -62,6 +62,7 @@ func NewBert(m *tf.SavedModel, vocabPath string, opts ...BertOption) (Bert, erro
 		inputFunc: func(inputs map[string]*tf.Tensor) estimator.InputFunc {
 			return func(m *tf.SavedModel) map[tf.Output]*tf.Tensor {
 				return map[tf.Output]*tf.Tensor{
+					//	m.Graph.Operation(UniqueIDsOp).Output(0):    inputs[UniqueIDsOp],
 					m.Graph.Operation(InputIDsOp).Output(0):     inputs[InputIDsOp],
 					m.Graph.Operation(InputMaskOp).Output(0):    inputs[InputMaskOp],
 					m.Graph.Operation(InputTypeIDsOp).Output(0): inputs[InputTypeIDsOp],
@@ -70,11 +71,11 @@ func NewBert(m *tf.SavedModel, vocabPath string, opts ...BertOption) (Bert, erro
 		},
 		modelFunc: func(m *tf.SavedModel) ([]tf.Output, []*tf.Operation) {
 			return []tf.Output{
-					m.Graph.Operation(InputIDsOp).Output(0),
-					m.Graph.Operation(InputMaskOp).Output(0),
-					m.Graph.Operation(InputTypeIDsOp).Output(0),
+					m.Graph.Operation(EmbeddingOp).Output(0),
+					//		m.Graph.Operation("feature_ids").Output(0),
 				},
-				nil
+				[]*tf.Operation{
+				}
 		},
 	}
 	for _, opt := range opts {
