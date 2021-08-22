9db2754e3d7a5118a0470f0189686ccd3ee4efd3
Kaby Chow <kaby.chow@gmail.com>
2021-08-22 14:29:59 +0800

    update

--- model/bert.go
+++ model/bert.go
@@ -70,7 +70,9 @@ func NewBert(m *tf.SavedModel, vocabPath string, opts ...BertOption) (Bert, erro
 		},
 		modelFunc: func(m *tf.SavedModel) ([]tf.Output, []*tf.Operation) {
 			return []tf.Output{
-					m.Graph.Operation("feature_ids").Output(0),
+					m.Graph.Operation(InputIDsOp).Output(0),
+					m.Graph.Operation(InputMaskOp).Output(0),
+					m.Graph.Operation(InputTypeIDsOp).Output(0),
 				},
 				nil
 		},
